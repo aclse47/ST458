875de3cdd90cd8490a517f66c86800041878c632
anniec47 <v.chapman@lse.ac.uk>
2025-03-24 08:47:42 +0000

    Update LGBM_backtesting.R

--- ST458_R_project/LGBM_backtesting.R
+++ ST458_R_project/LGBM_backtesting.R
@@ -14,14 +14,111 @@ source('training_functions.R')
 source('feature_engineering_functions.R')
 source('model_evaluation_functions.R')
 
+################################################################################
+# Data Preprocessing & Cointegration 
+################################################################################
+
 df <- read.csv('df_train.csv')                    # Read in csv
 df$date <- as.Date(df$date, format = "%Y-%m-%d")  # Make the date column date instead of char
 df <- df %>% arrange(symbol, date)                # Order according to symbol then date like in case study lecture
+
+df_wide <- df %>%                        # convert to wide so that each column = stock closing price over time
+  dplyr::select(date, symbol, close) %>% # close price 
+  tidyr::pivot_wider(names_from = symbol, values_from = close)
+df_wide <- as.data.frame(df_wide)
+
+rownames(df_wide) <- df_wide$date
+df_wide$date <- NULL
+
+df_wide <- df_wide[, sapply(df_wide, is.numeric), drop = FALSE]
+
+df_wide <- df_wide %>% mutate(across(everything(), ~ zoo::na.locf(.x, na.rm = FALSE))) # if price is NA replace w last known price (cannot have NA for Johansen)
+
+# Johansen Test & VECM
+num_assets_per_group <- 5 
+johansen_results <- list()
+vecm_models <- list()
+residuals_list <- list()
+asset_names <- colnames(df_wide)
+
+# loop thru assets and get a subset 
+for (i in seq(1, length(asset_names), by = num_assets_per_group)) {
+  asset_subset <- asset_names[i:min(i + num_assets_per_group - 1, length(asset_names))]
+  df_subset <- df_wide[, asset_subset, drop = FALSE]
+  
+  if (any(is.na(df_subset))) next  
+  
+  # Johansen 
+  johansen_test <- ca.jo(df_subset, type = "trace", ecdet = "none", K = 2) # trace stat for cointegration
+  trace_stat <- johansen_test@teststat 
+  critical_values <- johansen_test@cval
+  
+  significant_ranks <- which(trace_stat > critical_values[, 2])  # 5% level
+  significant_ranks <- ifelse(length(significant_ranks) == 0, NA, length(significant_ranks)) # if no rank, set NA
+  
+  johansen_results[[paste(asset_subset, collapse = ", ")]] <- list(
+    Trace_Stats = trace_stat,
+    Significant_Ranks = significant_ranks
+  )
+  
+  # VECM if cointegration is found
+  if (!is.na(significant_ranks) && significant_ranks > 0) {
+    vecm_model <- cajorls(johansen_test, r = significant_ranks)
+    vecm_models[[paste(asset_subset, collapse = ", ")]] <- vecm_model
+    
+    vecm_residuals <- residuals(vecm_model$rlm)
+    residuals_df <- as.data.frame(vecm_residuals)
+    
+    #  align residuals with dates
+    if (nrow(residuals_df) != nrow(df_wide)) {
+      residuals_df$date <- tail(df$date, nrow(residuals_df))
+    } else {
+      residuals_df$date <- df$date
+    }
+    
+    colnames(residuals_df)[1:ncol(vecm_residuals)] <- paste0(asset_subset[1:ncol(vecm_residuals)], "_residual")
+    
+    residuals_list[[paste(asset_subset, collapse = ", ")]] <- residuals_df
+  }
+}
+
+if (length(residuals_list) > 0) {
+  all_residuals <- Reduce(function(x, y) full_join(x, y, by = "date"), residuals_list)
+} else {
+  all_residuals <- data.frame(date = df$date)
+}
+
+head(all_residuals)
+
+residuals_df <- as.data.frame(all_residuals)
+residuals_df$date <- as.Date(residuals_df$date, format = "%Y-%m-%d")
+
+residuals_long <- residuals_df %>%
+  pivot_longer(-date, names_to = "symbol", values_to = "residual") %>%
+  mutate(symbol = gsub("_residual", "", symbol))
+
+residuals_all <- residuals_long %>% # adding lag 1 and lag 2 residuals
+  group_by(symbol) %>%
+  arrange(date, .by_group = TRUE) %>%
+  mutate(
+    residual_lag1 = lag(residual, 1),
+    residual_lag2 = lag(residual, 2)
+  ) %>%
+  ungroup()
+
+df_with_residuals <- df %>%
+  left_join(residuals_all, by = c("date", "symbol"))                              
+
+head(df_with_residuals[df_with_residuals$symbol == "TPLF", ]) # check to see if works
 
 tickers <- unique(df$symbol)
 
 df_with_features <- add_features(df, dV_kalman = 10, dW_kalman = 0.0001)
 df_with_features <- as.data.frame(df_with_features)
+
+################################################################################
+# LGBM
+################################################################################
 
 response_vars <- colnames(df_with_features %>% dplyr::select(matches("fwd")))
 covariate_vars <- setdiff(colnames(df_with_features), c(response_vars, 'date', 'symbol'))
@@ -31,7 +128,6 @@ covariate_vars <- setdiff(colnames(df_with_features), c(response_vars, 'date', '
 
 categorical_vars <- c()
 
-
 df_with_features_train <- df_with_features[df_with_features$date < as.Date('2013-01-01'), ]
 df_with_features_test <- df_with_features[df_with_features$date >= as.Date('2013-01-01'), ]
 
@@ -39,7 +135,7 @@ df_with_features_test <- df_with_features[df_with_features$date >= as.Date('2013
 param_df <- expand.grid(
   train_length = c(252, 252*2, 126),
   valid_length = c(21, 63),
-  lookahead = c(21),
+  lookahead = c(5),
   num_leaves = c(5,10,50),
   min_data_in_leaf = c(250,1000),
   learning_rate = c(0.01,0.03,0.1),
@@ -67,9 +163,6 @@ lgbm_features_effects_plot(df_with_features_train, covariate_vars, training_log[
 lgbm_hyperparameters_marginal_effect_plot(training_log)$lookahead
 dev.off()
 lgbm_hyperparameters_marginal_effect_plot(lookahead$training_log)
-plots <- lgbm_hyperparameters_marginal_effect_plot(training_log)
-lookahead_plot <- plots$lookahead
-print(lookahead_plot)
 
 ################################################################################
 # Back-testing a trading algorithm on the validation set.
